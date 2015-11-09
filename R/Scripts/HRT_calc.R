8ecf6d7e2031276799d15d4aed5162c55da2a489
Valeria Blesius <valeria.blesius@mni.thm.de>
2015-11-09 14:14:02 +0100

    interval variable names changed

--- R/Scripts/HRT_calc.R
+++ R/Scripts/HRT_calc.R
@@ -4,25 +4,27 @@ chf201 = read.table(file.path(path , "chf201.ecg.txt"))
 chf201 = unlist(chf201*1000)
 
 require(zoo)
-rollapply(chf201, 26, checkForPVC) # 10 RR needed before PVC, 15 after PVC = 26, see http://www.h-r-t.com/hrt/en/calc.html
+rollapply(chf201, 22, checkForPVC) # 5 RR needed before PVC, 1 compensatory interval, 15 after PVC = 26, see http://www.h-r-t.com/hrt/en/calc.html
 # Stattdessen
 ## wapply? http://www.r-bloggers.com/wapply-a-faster-but-less-functional-rollapply-for-vector-setups/ 
 ## RcppRoll package?
 
-checkForPVC = function(x) {
+checkForPVC = function(x) { 
   
-  PVC = x[11]
-  prePVC = x[10]
-  postPVC = x[12]
-  pre5PVCs = x[6:10]
+  i_coupl= x[6] # coupling interval
+  i_comp = x[7] # compensatory interval
+  i_pre = x[1:5] # preceding intervals
+  i_post = x[8:22] # following intervals (after comp. interval)
+  
+  ref = mean(i_pre)
 
     # checks for PVC
-  if (PVC <= prePVC*0.8) { # Prematurity of 20%
-    if (postPVC >= refLength*1.2) { # Lengthening of post PVC interval
+  if (i_coupl <= ref*0.8) { # Prematurity of 20%
+    if (i_comp >= ref*1.2) { # Lengthening of post PVC interval
       
       # checks for arrhythmias and artefacts
-      if (all(pre5PVCs > 300)) {
-        if (all(pre5PVCs < 2000)) {
+      if (all(i_pre > 300)) {
+        if (all(i_pre < 2000)) {
           if (all(rollapply())) {
             
           }

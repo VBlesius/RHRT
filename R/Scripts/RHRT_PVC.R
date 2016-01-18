d2a0e15a0dc8720b2846266c5b6591bcc7bcb3d9
Valeria Blesius <valeria.blesius@mni.thm.de>
2016-01-18 10:47:48 +0100

    added abline (regression line) to plot

--- R/Scripts/RHRT_PVC.R
+++ R/Scripts/RHRT_PVC.R
@@ -5,7 +5,8 @@ PVC <- setClass("PVC",
                  preRRs="vector",
                  postRRs="vector",
                  TO="numeric",
-                 TS="numeric")
+                 TS="numeric",
+                 ablineCoefficients="vector")
 )
 
 
@@ -21,6 +22,15 @@ setMethod("getHRTParameters", "PVC", function(thisObject) {
   slopes <- wapply(postRRs, 5, by=1, FUN=function(y) return(lm(y ~ seq(1,5))$coefficients[2]))
   thisObject@TS <- max(slopes, na.rm = TRUE)
   
+  # Calculate coefficients for regression line in plot
+  index <- which.max(slopes)
+  model <- lm(postRRs[index:(index+4)]~seq(1,5))
+  
+  slope <- model$coefficients[2]
+  intercept <- model$coefficients[1]-(slope*(3+index)) # 3 = #preRRs + #irregular RRs - 1
+  
+  thisObject@ablineCoefficients <- c(intercept, slope)
+
   return(thisObject)
 })
 
@@ -42,4 +52,8 @@ setMethod("plot", "PVC", function(x, type="cropped") {
          "o", pch=20, 
          ylim = c(mean(RRs)-sd(RRs)/2, mean(RRs)+sd(RRs)/2),
          xlab="# of RR interval", ylab="length of RR interval (ms)")
-  }})
+  }
+ 
+  abline(coef=x@ablineCoefficients)
+
+})

46ea4de8a54f1547e1e9642c876958aebcb15793
Valeria Blesius <valeria.blesius@mni.thm.de>
2016-02-25 07:25:02 +0100

    adapted example.R for common use

--- R/Scripts/RHRT/examples/example.R
+++ R/Scripts/RHRT/examples/example.R
@@ -1,7 +1,7 @@
 # Load input data
 args <- commandArgs(TRUE)
 suppressWarnings(tryCatch(
-  data <- read.table(file.path(getwd(), args[1])),
+  data <- read.table(args[1]),
   error = function(e) {
     stop("File could not be found! Path incorrect or file not existing!
       Please try again and pass a valid relative path!", call. = FALSE)
@@ -10,15 +10,12 @@ suppressWarnings(tryCatch(
 data <- unlist(data * 1000)
 
 # Load dependencies
-library(methods)
-sourcepath <- "/home/etz/Documents/Promotion/"
-source(file.path(sourcepath, "R/Scripts/wapply.R"))
-source(file.path(sourcepath, "R/Scripts/RHRT_Functions.R"))
-source(file.path(sourcepath, "R/Scripts/RHRT_PVC.R"))
+library(RHRT)
 
 # Pipeline
 pvcs <- get_pvcs(data)
-averaged_pvc <- calc_averaged_pvc()
+pvcs <- sapply(pvcs, get_hrt_params)
+averaged_pvc <- calc_averaged_pvc(pvcs)
 averaged_pvc <- get_hrt_params(averaged_pvc)
 
 # Output

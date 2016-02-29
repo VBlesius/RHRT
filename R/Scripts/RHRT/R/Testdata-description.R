490b043c0ceba43e8b21da4dd7edc3098b37e69e
Valeria Blesius <valeria.blesius@mni.thm.de>
2016-02-29 07:27:45 +0100

    description of rda-testdataset

---
+++ R/Scripts/RHRT/R/Testdata-description.R
@@ -0,0 +1,17 @@
+#' Intervals without noise
+#'
+#' Interval data based on a dataset provided by Physionet (NSRDB). This dataset
+#' includes 5 HRTs that fit the HRT filter rules. For more information see the
+#' vignette.
+#'
+#' @format A numeric vector of length 1000.
+"testdata_regular"
+
+#' Intervals with noise
+#'
+#' Interval data based on a dataset provided by Physionet (NSRDB). This dataset
+#' includes 3 HRTs that fit the HRT filter rules. For more information see the
+#' vignette
+#'
+#' @format A numeric vector of length 1000.
+"testdata_variant"

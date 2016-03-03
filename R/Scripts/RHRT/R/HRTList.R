07772c566d5ed20dfb01b715f7f0e083bc13dbd6
Valeria Blesius <valeria.blesius@mni.thm.de>
2016-03-03 10:09:48 +0100

    created a HRTList class

---
+++ R/Scripts/RHRT/R/HRTList.R
@@ -0,0 +1,17 @@
+#' S4 class to represent a list of HRT objects
+#'
+#' This class specifies an object to save all HRT objects of a given vector. It 
+#' also saves an averaged HRT for calculation of the averaged HRT parameters and
+#' plotting of all HRTs in a single plot.
+#'
+#' @slot HRTs list, all HRT objects
+#' @slot avHRT HRT, averaged HRT
+#' 
+#' @rdname HRTList
+#' 
+#' @export
+setClass("HRT",
+         slots = list(
+           HRTs = "list",
+           avHRT = "HRT"),
+)

8ad8eb6cc09aecde081ed11279b3032d11fb20ad
Valeria Blesius <valeria.blesius@mni.thm.de>
2016-03-03 16:53:39 +0100

    example adapted to new package style

--- R/Scripts/RHRT/data-raw/example.R
+++ R/Scripts/RHRT/data-raw/example.R
@@ -1,23 +1,7 @@
-# Load input data
-args <- commandArgs(TRUE)
-suppressWarnings(tryCatch(
-  data <- read.table(args[1]),
-  error = function(e) {
-    stop("File could not be found! Path incorrect or file not existing!
-      Please try again and pass a valid relative path!", call. = FALSE)
-  }
-))
-data <- unlist(data * 1000)
-
 # Load dependencies
 library(RHRT)
 
-# Pipeline
-hrts <- getHRTs(data)
-hrts <- sapply(hrts, getHRTParams)
-averagedHrt <- calcAveragedHRT(hrts)
-averagedHrt <- getHRTParams(averagedHrt)
-
-# Output
-cat(length(hrts), "\t", mean(sapply(hrts, slot, "TO")), "\t", mean(sapply(hrts, slot, "TS")), "\n")
-cat("Average", "\t", averagedHrt@TO, "\t", averagedHrt@TS, "\n")
+tdVariant <- unname(testdataVariant)
+hrtVar <- vectorToHRT(tdVariant)
+plot(hrtVar)
+hrtVarParams <- getParamsMean(hrtVar)

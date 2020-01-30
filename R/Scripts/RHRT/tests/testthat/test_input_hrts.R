fe9f326d633a9494aca8626bd5ca9a1fb63e39d1
Valeria Blesius <valeria.blesius@mni.thm.de>
2020-01-30 10:26:13 +0100

    RHRT: shortens test formulation

--- R/Scripts/RHRT/tests/testthat/test_input_hrts.R
+++ R/Scripts/RHRT/tests/testthat/test_input_hrts.R
@@ -6,10 +6,10 @@ data("testdataVariant")
 data("testdataVariantNoHRT")
 
 test_that("Finding HRTs", {
-  expect_equal(length(vectorToHRT(testdataRegular)@HRTs), 5)
-  expect_equal(length(vectorToHRT(testdataVariant)@HRTs), 3)
-  expect_equal(length(vectorToHRT(rep(782, 1000))@HRTs), 0)
-  expect_equal(length(vectorToHRT(testdataVariantNoHRT)@HRTs), 0)
+  expect_length(vectorToHRT(testdataRegular)@HRTs, 5)
+  expect_length(vectorToHRT(testdataVariant)@HRTs, 3)
+  expect_length(vectorToHRT(rep(782, 1000))@HRTs, 0)
+  expect_length(vectorToHRT(testdataVariantNoHRT)@HRTs, 0)
      
   expect_warning(vectorToHRT(rep(782, 1000)), "No HRTs")
   expect_warning(vectorToHRT(testdataVariantNoHRT), "No HRTs")

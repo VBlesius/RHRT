b84a8351cbb37c75c91548ce803929c0858f633e
Valeria Blesius <valeria.blesius@mni.thm.de>
2021-04-26 16:51:35 +0200

    RHRT: updates testdata

--- R/Scripts/RHRT-package/RHRT/R/Testdata-description.R
+++ R/Scripts/RHRT-package/RHRT/R/Testdata-description.R
@@ -22,12 +22,28 @@
 #' does not include any HRTs. For more information see the vignette.
 #'
 #' @format A numeric vector of length 1000.
-"testdataVariantNoHRT" 
+"testdataVariantNoHRT"
 
-#' Valid HRTList object
+#' Intervals without noise
 #'
-#' A HRTList object based on a dataset provided by Physionet (NSRDB). This object
-#' includes 3 HRTs that result in a significant HRTA class.
+#' HRTList Object that was generated from testdataRegular and includes 5
+#' identical HRTs. For more information see the vignette.
 #'
-#' @format A HRTList with 3 HRTs
-"HRTs"
+#' @format An HRTList object.
+"testdataRegular_HRTObj"
+
+#' Intervals with noise
+#'
+#' HRTList Object that was generated from testdataVariant and includes 3 HRTs
+#' based on data from Physionet (NSRDB). For more information see the vignette.
+#'
+#' @format An HRTList object.
+"testdataVariant_HRTObj"
+
+#' Intervals with noise and without HRT
+#'
+#' HRTList Object that was generated from testdataVariantNoHRT. This HRTList
+#' does not include any HRT. For more information see the vignette.
+#'
+#' @format  An empty HRTList object.
+"testdataVariantNoHRT_HRTObj"

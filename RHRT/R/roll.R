9e81690874069549e68d21a8f9ac53228e812a0a
Valeria Blesius <valeria.blesius@mni.thm.de>
2021-05-28 11:58:55 +0200

    written new function roll (to substitute wapply)

---
+++ RHRT/R/roll.R
@@ -0,0 +1,23 @@
+#' Apply method on sliding window
+#' 
+#' Applies a given function on a vector by rolling over it with a sliding window 
+#' mechanism.
+#' 
+#' This method was inspired by the function "wapply" by A. N. Spiess, University
+#' Hospital Hamburg-Eppendorf (https://rmazing.wordpress.com/2013/04/23/wapply-a-faster-but-less-functional-rollapply-for-vector-setups/),
+#' but adjusted for this package to speed it up.
+#' 
+#' @param intervals vector
+#' @param width windowsize
+#' @param fun function to be applied
+#' @param ... additional arguments for FUN
+#' @return list with return values of fun for each window
+roll <- function(intervals, width, fun, ...) {
+  width = width-1
+  l <- length(intervals)
+  indicesStart <- seq(1, l-width)
+  indicesEnd <- seq(1+width, l)
+  lapply(indicesStart, function(i) {
+    fun(intervals[indicesStart[i]:indicesEnd[i]], ...)
+  })
+}

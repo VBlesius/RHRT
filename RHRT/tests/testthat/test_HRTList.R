8b981055d00ae31ac38f8ab3e9d983c01b8ffba2
Valeria Blesius <valeria.blesius@mni.thm.de>
2021-06-05 13:16:01 +0200

    adds tests for not yet tested functions / parameter combinations

---
+++ RHRT/tests/testthat/test_HRTList.R
@@ -0,0 +1,11 @@
+library(RHRT)
+context("HRTList functions")
+
+load(test_path("testdata", "testdataVariant_HRTObj.rda"))
+testdataVariant_HRTObj2 <- testdataVariant_HRTObj
+testdataVariant_HRTObj2@pos <- NA_real_
+
+test_that("Positions", {
+  expect_equal(getPositions(testdataVariant_HRTObj), testdataVariant_HRTObj@pos)
+  expect_error(getPositions(testdataVariant_HRTObj2), "There seem to be no HRTs in your HRTList")
+})

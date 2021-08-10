2aeba4cf710745fecd91b4a64b451cde8907b36c
Valeria Blesius <valeria.blesius@mni.thm.de>
2021-08-10 12:00:00 +0200

    adds copyright and licence notices to each source file

--- RHRT/R/HRTList.R
+++ RHRT/R/HRTList.R
@@ -1,3 +1,20 @@
+###-----------------------------------------------------------------------------
+# Part of RHRT: R package to assess Heart Rate Turbulence from RR interval data 
+# Copyright (C) 2021 Valeria Blesius
+
+# RHRT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 only.
+
+# RHRT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with RHRT.  If not, see <https://www.gnu.org/licenses/>.
+###-----------------------------------------------------------------------------
+
 #' S4 class to represent a list of HRT objects
 #'
 #' This class specifies an object to save all HRT objects of a given vector. It
